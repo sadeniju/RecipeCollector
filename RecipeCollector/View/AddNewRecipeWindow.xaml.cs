7550d9efa328c88c07fb64fcb7b0d017efc593a3
sadeniju <sade@niju.fake>
2014-04-12 22:42:46 +0200

    Added wireframes and the collected ideas document

---
+++ RecipeCollector/View/AddNewRecipeWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RecipeCollector.View
+{
+    /// <summary>
+    /// Interaktionslogik für AddNewRecipeWindow.xaml
+    /// </summary>
+    public partial class AddNewRecipeWindow : Window
+    {
+        public AddNewRecipeWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
